237f2222faedd8894b285e6a3e0108e2d92dcda7
Nam Hoài Nguyễn <nam.nguyen@adnovum.sg>
2022-11-08 11:12:17 +0800

    style: change commitlint to bypass footer, subject and type rules.

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,5 +3,11 @@ module.exports = {
   rules: {
     'subject-case': [2, 'never', ['start-case', 'pascal-case', 'upper-case']],
     'scope-case': [2, 'always', ['pascal-case', 'lower-case', 'camel-case']],
+    'footer-max-line-length': [0],
+    'subject-empty': [0],
+    'type-empty': [0],
+    'subject-full-stop': [0],
+    'body-leading-blank': [0],
+    'footer-leading-blank': [0],
   },
 }
